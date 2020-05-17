03422b0c5514524544eef0d239dd4a7b1e17468b
Ola-M <aleksandra.mikol99@gmail.com>
2020-05-18 01:51:51 +0200

    18.05.2020

--- project1/src/main/java/com/example/project1/accessToData/FilmRepository.java
+++ project1/src/main/java/com/example/project1/accessToData/FilmRepository.java
@@ -2,8 +2,16 @@ package com.example.project1.accessToData;
 
 import com.example.project1.accessToData.table.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RepositoryRestResource
+import java.util.List;
+
+@Repository
 public interface FilmRepository extends JpaRepository<Film,Long> {
+
+   @RestResource(path="findFilm",rel="findFilm")
+   List<Film> findByTitleContaining(@RequestParam String title);
+
 }
--- project1/src/main/java/com/example/project1/manager/FilmManager.java
+++ project1/src/main/java/com/example/project1/manager/FilmManager.java
@@ -3,28 +3,37 @@ package com.example.project1.manager;
 import com.example.project1.accessToData.FilmRepository;
 import com.example.project1.accessToData.table.Film;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.util.Optional;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
 
 @Service
 public class FilmManager {
 
-    private FilmRepository filmRepository;
+    private final FilmRepository filmRepository;
 
     @Autowired
     public FilmManager(FilmRepository filmRepository) {
         this.filmRepository = filmRepository;
 
     }
-
-    public Optional<Film> findById (Long id) {
-        return this.filmRepository.findById(id);
+    //wtszukuje po tytule
+    public List<Film> findFilm(String title){
+        return this.filmRepository.findByTitleContaining(title);
     }
+
+    public boolean exist(Long id){
+        return this.filmRepository.existsById(id);
+    }
+
+
+    /*public Page<Film> findAll (Pageable pegable) {
+        return this.filmRepository.findAll(pegable);
+    }*/
 
     public Iterable<Film> findAll () {
         return this.filmRepository.findAll();
@@ -33,18 +42,19 @@ public class FilmManager {
     public Film save(Film film) {
         return this.filmRepository.save(film);
     }
+
+
     public void deleteById(Long id) {
        this.filmRepository.deleteById(id);
     }
 
-    //jeszcez dopisze się metode która wyświrtla filmy posortowane po nazwie ale to potem
-    //bo nie che mi sie teraz szukać        Lista < T > findAll ( Sort  sort)
 
 
-    //przy oruchominiu aplikacji dodaje film do bazy
+
+ /*   //przy oruchominiu aplikacji dodaje film do bazy
     @EventListener(ApplicationReadyEvent.class)
     public void saveDB(){
         save(new Film("Matrix", LocalDate.of(1999, 3,24),"najlepszy","link" ));
-    }
+    }*/
 
 }
