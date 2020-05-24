ec0a3fc3c6ee24975b572b4c9a195896957ef909
Skrabka98 <barti.sk@gmail.com>
2020-05-24 19:47:41 +0200

    xx

--- project1/src/main/java/com/example/project1/api/FilmControler.java
+++ project1/src/main/java/com/example/project1/api/FilmControler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FilmControler {
 
     @GetMapping
-    public String showFilms(){
-        return "films";
+    String showFilms(){
+        return "projects";
     }
 }
