84d9cdd3116ab57178e8674b08bf69677a4c6a5b
Skrabka98 <barti.sk@gmail.com>
2020-05-26 21:56:40 +0200

    26.05
    naprawione

--- project1/src/main/java/com/example/project1/controller/FilmIndexControler.java
+++ project1/src/main/java/com/example/project1/controller/FilmIndexControler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping(value = "/index")
+@RequestMapping(value = "/")
 public class FilmIndexControler {
     private final FilmService filmService;
 
