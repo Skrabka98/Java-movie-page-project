dc8cfb8c84a46a360385d5854cb07aeab56257e6
Ola-M <aleksandra.mikol99@gmail.com>
2020-05-30 21:43:29 +0200

    bkb

---
+++ project1/src/main/java/com/example/project1/security/SsoController.java
@@ -0,0 +1,20 @@
+package com.example.project1.security;
+
+import com.example.project1.controller.FilmIndexControler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.rmi.ServerException;
+
+@Controller
+public class SsoController {
+    @GetMapping("/logout")
+    String logout(HttpServletRequest request, FilmIndexControler film) throws ServletException{
+        request.logout();
+        return "index";
+    }
+
+}
